db62b70e14f884ce463b304da8b1550563c685c8
ygha <yghafivehome@gmail.com>
2021-03-06 15:34:19 +0900

    first commit

---
+++ MPAndroidChart-master/MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/MyXAxisValueFormatter.java
@@ -0,0 +1,15 @@
+package com.xxmassdeveloper.mpchartexample;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+public class MyXAxisValueFormatter implements IAxisValueFormatter {
+    private String[] mValues;
+    public MyXAxisValueFormatter(String[] values) {
+        this.mValues = values;
+    }
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return mValues[(int) value];
+    }
+}
